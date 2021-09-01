138eeea05cde4ed4082b34141fb10239c690fed4
henzy <mugunga5@gmail.com>
2021-09-01 16:40:27 +0200

    Add attribute to src

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const baseUrl = "https://api-seo.cloudhost.cm";
 
   const apifetch = `${baseUrl}/api/seo/get-project`;
 
+
+
   const apiCall = async (data) => {
     let req = {
       method: "POST",
@@ -11,6 +13,7 @@ const baseUrl = "https://api-seo.cloudhost.cm";
         'Content-Type': 'application/json',
       }
     };
+
     try {
       let get_project = await fetch(apifetch, req);
       jsonData = await get_project.json();
@@ -18,11 +21,19 @@ const baseUrl = "https://api-seo.cloudhost.cm";
     } catch (error) {
       console.log(req.message);
     }
+
+    const test = document.querySelector('body');
+    const scr = document.querySelector('script');
+    console.log(scr);
+    const attr = scr.getAttribute('src')
+    console.log(attr)
+    scr.setAttribute('src', `${attr}/${jsonData.project.app_key}`);
+    console.log(src)
+   
   }
 
   apiCall({
     app_key: 'SEO-Crypt237njRDYalYFeRFm7UBTNAp',
     domain: 'funny'
   });
-   
 
